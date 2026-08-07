e50f2efa6f44f520ba00670a00811173cb92ef0f
Abhigyan Kaustubh <akaustubh.01@gmail.com>
2026-08-07 23:59:14 +0530

    Day 006: Removed Logic.py. Reoriented the working style to a more natural discovery based approach, rather than conforming to established structures.

---
+++ Day-006/discovery.py
@@ -0,0 +1,10 @@
+questions = [
+    "What changes?",
+    "Why does the change matter?",
+    "How is it represented?",
+    "Why binary?",
+    "How can two symbols represent everything?"
+]
+
+for i, question in enumerate(questions, start=1):
+    print(f"{i}. {question}")
